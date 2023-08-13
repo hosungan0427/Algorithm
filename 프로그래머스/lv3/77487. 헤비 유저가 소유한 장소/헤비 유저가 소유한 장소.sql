91cec753bb33a36a8623a96f835310725349e89a
hosungan0427 <140784941+hosungan0427@users.noreply.github.com>
2023-08-13 15:25:16 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT * FROM PLACES
+WHERE HOST_ID IN (SELECT HOST_ID FROM PLACES
+                    group by HOST_ID
+                    HAVING COUNT(HOST_ID) >= 2
+                 )
+ORDER BY ID

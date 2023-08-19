d7e12283eaf39c40205c1b62842c61e02ec23a76
hosungan0427 <140784941+hosungan0427@users.noreply.github.com>
2023-08-19 14:38:48 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,11 +1,9 @@
-import java.util.regex.Pattern;
-
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
 
         if(s.length()==4 || s.length()==6){
-            return answer= Pattern.matches("^[0-9]*$",s);
+            return answer= s.matches("^[0-9]*$");
             }else{
             answer = false;
         }
